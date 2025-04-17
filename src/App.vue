861f790f1a03b7ad8a1967ff860274edea929881
liyb <1055025755@qq.com>
2025-04-17 10:09:14 +0800

    add eruda debugger

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import { onLaunch, onShow, onHide } from '@dcloudio/uni-app';
+
+const VITE_APP_ENV = import.meta.env.VITE_APP_ENV;
+// 区分环境引用eruda
+if (VITE_APP_ENV !== 'prod') {
+  import('eruda').then((eruda) => {
+    eruda?.default?.init();
+  });
+}
+
 onLaunch(() => {
   console.log('App Launch');
 });
